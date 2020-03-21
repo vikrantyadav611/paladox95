e3de49e093e55a57846d91029b40295e56d6d2c7
vikrant <yadavvikrant30oct@gmail.com>
2020-03-22 00:20:29 +0530

    new update

---
+++ src/Component/AboutModal.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import { Tabs, Table, TableHead, TableDataCell, TableRow, TableBody, TableHeadCell, Tab, TabBody, Window, WindowHeader, WindowContent, Avatar, Button, List, ListItem } from 'react95'
+import '../Component/AboutModal.css'
+
+export default function MyTabs(props) {
+
+    const { closemodel } = props
+
+    return (
+
+
+        <Window>
+
+            <WindowHeader
+                style={{
+                    justifyContent: 'space-between',
+                    display: 'flex'
+                }}
+            >
+                <span>About</span>
+                <Button size='sm' onClick={closemodel} style={{ cursor:'default', marginTop: '3px', marginRight: '-2px' }}>
+                   <span className='closebtn'> x</span>
+                    </Button>
+            </WindowHeader>
+            <WindowContent style={{ width: 300,textAlign:'center' }}>
+                <h2>PalaDox95</h2>
+                 <strong>An open source Windows 95 style Paladox.</strong>
+                    <br/>
+                 <strong>Built with React, React95.</strong>
+                    <br/>
+                
+                <strong>Made with ❤️ by <a href='https://twitter.com/Breadistaken' target='_blank'>@dreddd</a></strong>
+                    
+
+
+            </WindowContent>
+        </Window>
+
+
+    )
+}
---
+++ src/Component/ChampModal.js
@@ -0,0 +1,170 @@
+import React, { useState, useEffect } from 'react'
+import { Tabs, Table, TableHead, TableDataCell, TableRow, TableBody, TableHeadCell, Tab, TabBody, Window, WindowHeader, WindowContent, Avatar, Button, List, ListItem } from 'react95'
+import { Container, Row, Col } from 'react-bootstrap'
+import ReactImg from 'react-image'
+import '../Component/ChampModal.css'
+
+export default function MyTabs(props) {
+    const [myvalue, setMyvalue] = useState(0)
+    // const [newdata,setNewdata]=useState([])
+    const { data, showmymodal } = props
+    // newdata.push(data[iconindex])
+    console.log(data)
+    const changetab = (value) => {
+        setMyvalue(value)
+    }
+
+
+    return (
+
+        data.map(item => (
+
+            <Window style={{ height: 400, width: 600 }}>
+
+                <WindowHeader
+                    style={{
+                        justifyContent: 'space-between',
+                        display: 'flex'
+                    }}
+                >
+                    <span>Champion.exe</span>
+                    <Button size={'sm'} style={{cursor:'default', marginLeft: -3, marginTop: 3 }} onClick={showmymodal} >
+                    <span className='closebtn'>x
+                    </span>
+                    </Button>
+                </WindowHeader>
+                <WindowContent >
+                    <Tabs value={myvalue} onChange={changetab} style={{ marginBottom: '30' }}>
+                        <Tab value={0}>Basic Details</Tab>
+                        <Tab value={1}>Abilities</Tab>
+                        <Tab value={2}>Lore</Tab>
+                    </Tabs>
+                    {
+                        myvalue === 0 &&
+                        <TabBody>
+                            <Container>
+                                <Row>
+                                    <Col>
+                                        <ReactImg style={{ width: 140, height: 140 }}
+                                            src={item.ChampionIcon_URL}
+                                            loader={
+                                                <img
+                                                    alt="placeholder"
+                                                    style={{ width: 50 }}
+                                                    src={'pala_1.png'}
+                                                />
+                                            }
+                                        />
+                                    </Col>
+                                    <Col>
+                                        <List fullWidth='true'>
+                                            <ListItem>Name: <strong>{item.Name}</strong></ListItem>
+                                            <ListItem>Role: <strong>{item.Roles}</strong></ListItem>
+                                            <ListItem>Health: <strong>{item.Health}</strong> </ListItem>
+                                            <ListItem>Speed: <strong>{item.Speed}</strong> </ListItem>
+                                        </List>
+                                    </Col>
+                                </Row>
+                            </Container>
+
+
+
+                        </TabBody>
+                    }
+                    {
+                        myvalue === 1 &&
+                        <TabBody>
+                            <List fullWidth='true'>
+                                <ListItem>
+                                    <Row>
+                                        <Col>
+                                            <ReactImg style={{ width: 30, height: 30 }}
+                                                src={item.ChampionAbility1_URL}
+                                                loader={
+                                                    <img
+                                                        alt="placeholder"
+                                                        style={{ width: 30 }}
+                                                        src={'pala_1.png'} />
+                                                }
+                                            />
+                                        </Col>
+                                        <Col><strong>{item.Ability1}</strong></Col>
+                                    </Row>
+                                </ListItem>
+                                <ListItem>
+                                    <Row>
+                                        <Col>
+                                            <ReactImg style={{ width: 30, height: 30 }}
+                                                src={item.ChampionAbility2_URL}
+                                                loader={
+                                                    <img
+                                                        alt="placeholder"
+                                                        style={{ width: 30 }}
+                                                        src={'pala_1.png'} />
+                                                }
+                                            />
+                                        </Col>
+                                        <Col><strong>{item.Ability2}</strong></Col>
+                                    </Row>
+                                </ListItem>
+                                <ListItem>
+                                    <Row>
+                                        <Col>
+                                            <ReactImg style={{ width: 30, height: 30 }}
+                                                src={item.ChampionAbility3_URL}
+                                                loader={
+                                                    <img
+                                                        alt="placeholder"
+                                                        style={{ width: 30 }}
+                                                        src={'pala_1.png'} />
+                                                }
+                                            />
+
+                                        </Col>
+                                        <Col><strong>{item.Ability3}</strong></Col>
+                                    </Row>
+                                </ListItem>
+                                <ListItem>
+                                    <Row>
+                                        <Col><ReactImg style={{ width: 30, height: 30 }}
+                                            src={item.ChampionAbility4_URL}
+                                            loader={
+                                                <img
+                                                    alt="placeholder"
+                                                    style={{ width: 30 }}
+                                                    src={'pala_1.png'} />
+                                            }
+                                        />
+                                        </Col>
+                                        <Col><strong>{item.Ability4}</strong></Col>
+                                    </Row>
+                                </ListItem>
+                                <ListItem><Row>
+                                    <Col><ReactImg style={{ width: 30, height: 30 }}
+                                        src={item.ChampionAbility5_URL}
+                                        loader={
+                                            <img
+                                                alt="placeholder"
+                                                style={{ width: 30 }}
+                                                src={'pala_1.png'} />
+                                        }
+                                    />
+                                    </Col>
+                                    <Col><strong>{item.Ability5}</strong></Col>
+                                </Row>
+                                </ListItem>
+                            </List>
+                        </TabBody>
+                    }
+                    {
+                        myvalue === 2 &&
+                        <TabBody>
+                            {item.Lore}
+                        </TabBody>
+                    }
+                </WindowContent>
+            </Window>
+        ))
+
+    )
+}
