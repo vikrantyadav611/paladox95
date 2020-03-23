3d0912364d10233e9e4c17ab082028b4f40a4881
vikrant <yadavvikrant30oct@gmail.com>
2020-03-23 15:49:15 +0530

    Abilities Addition

--- src/Component/ChampModal.js
+++ src/Component/ChampModal.js
@@ -3,9 +3,16 @@ import { Tabs, Tab, TabBody, Window, WindowHeader, WindowContent, Button, List, 
 import { Container, Row, Col } from 'react-bootstrap'
 import ReactImg from 'react-image'
 import '../Component/ChampModal.css'
+import ClickAwayListener from 'react-click-away-listener'
 
 export default function MyTabs(props) {
     const [myvalue, setMyvalue] = useState(0)
+    const [row_one,setRow_one]=useState(false)
+    const [row_two,setRow_two]=useState(false)
+    const [row_three,setRow_three]=useState(false)
+    const [row_four,setRow_four]=useState(false)
+    const [row_fifth,setRow_fifth]=useState(false)
+
     // const [newdata,setNewdata]=useState([])
     const { data, showmymodal } = props
     // newdata.push(data[iconindex])
@@ -13,12 +20,11 @@ export default function MyTabs(props) {
         setMyvalue(value)
     }
 
-
     return (
 
         data.map(item => (
 
-            <Window style={{ height: 400, width: 600 }}>
+            <Window style={{ height: 480, width: 600 }}>
 
                 <WindowHeader
                     style={{
@@ -41,7 +47,7 @@ export default function MyTabs(props) {
                     {
                         myvalue === 0 &&
                         <TabBody>
-                            <Container>
+                            <Container style={{marginTop:30}}>
                                 <Row>
                                     <Col>
                                         <ReactImg style={{ width: 140, height: 140 }}
@@ -70,12 +76,113 @@ export default function MyTabs(props) {
 
                         </TabBody>
                     }
-                    {
+                    {   
+                         row_one && 
+                         <ClickAwayListener onClickAway={()=>setRow_one(false)}>
+                        <TabBody>
+                            <Button style={{marginBottom:10}} onClick={()=>setRow_one(false)}>Go back</Button>
+                            <Container>
+                                <Row>
+                                    <Col style={{textAlign:'center'}}>
+                                   <img 
+                                    style={{width:150}}
+                                    src={item.ChampionAbility1_URL}
+                                    />
+                                    <br/>
+                                    <strong>{item.Ability1} </strong>
+                                    </Col>
+                                    <Col><p style={{textAlign:'justify'}}>{item.Ability_1.Description}</p></Col>
+                                </Row>
+                            </Container>
+                        </TabBody>
+                        </ClickAwayListener>
+                        ||
+                        row_two && 
+                        <ClickAwayListener onClickAway={()=>setRow_two(false)}>
+                       <TabBody>
+                       <Button style={{marginBottom:10}} onClick={()=>setRow_two(false)}>Go back</Button>
+                            <Container>
+                                <Row>
+                                    <Col style={{textAlign:'center'}}>
+                                   <img 
+                                    style={{width:150}}
+                                    src={item.ChampionAbility2_URL}
+                                    />
+                                    <br/>
+                                    <strong>{item.Ability2} </strong>
+
+                                    </Col>
+                                    <Col><p style={{textAlign:'justify'}}>{item.Ability_2.Description}</p></Col>
+                                </Row>
+                            </Container>
+                       </TabBody>
+                       </ClickAwayListener>
+                        ||
+                        row_three && 
+                        <ClickAwayListener onClickAway={()=>setRow_three(false)}>
+                       <TabBody>
+                       <Button style={{marginBottom:10}} onClick={()=>setRow_three(false)}>Go back</Button>
+                            <Container style={{position:'relative'}}>
+                                <Row>
+                                    <Col style={{textAlign:'center'}}>
+                                   <img 
+                                    style={{width:150}}
+                                    src={item.ChampionAbility3_URL}
+                                    />
+                                    <br/>
+                                    <strong>{item.Ability3} </strong>
+                                    </Col>
+                                    <Col><p style={{textAlign:'justify'}}>{item.Ability_3.Description}</p></Col>
+                                </Row>
+                            </Container>
+                       </TabBody>
+                       </ClickAwayListener>
+                       ||
+                        row_four && 
+                        <ClickAwayListener onClickAway={()=>setRow_four(false)}>
+                       <TabBody>
+                       <Button style={{marginBottom:10}} onClick={()=>setRow_four(false)}>Go back</Button>
+                            <Container style={{position:'relative'}}>
+                                <Row>
+                                    <Col style={{textAlign:'center'}}>
+                                   <img 
+                                    style={{width:150}}
+                                    src={item.ChampionAbility4_URL}
+                                    />
+                                    <br/>
+                                    <strong>{item.Ability4} </strong>
+                                    </Col>
+                                    <Col><p style={{textAlign:'justify'}}>{item.Ability_4.Description}</p></Col>
+                                </Row>
+                            </Container>
+                       </TabBody>
+                       </ClickAwayListener>
+                        ||
+                        row_fifth && 
+                        <ClickAwayListener onClickAway={()=>setRow_fifth(false)}>
+                       <TabBody>
+                       <Button style={{marginBottom:10}} onClick={()=>setRow_fifth(false)}>Go back</Button>
+                            <Container style={{position:'relative'}}>
+                                <Row>
+                                    <Col style={{textAlign:'center'}}>
+                                   <img 
+                                    style={{width:150}}
+                                    src={item.ChampionAbility5_URL}
+                                    />
+                                    <br/>
+                                    <strong>{item.Ability5} </strong>
+                                    </Col>
+                                    <Col><p style={{textAlign:'justify'}}>{item.Ability_5.Description}</p></Col>
+                                </Row>
+                            </Container>
+                       </TabBody>
+                       </ClickAwayListener>
+                       ||
                         myvalue === 1 &&
                         <TabBody>
                             <List fullWidth='true'>
                                 <ListItem>
-                                    <Row>
+                                    <Row onClick={()=>setRow_one(true)}>
                                         <Col>
                                             <ReactImg style={{ width: 30, height: 30 }}
                                                 src={item.ChampionAbility1_URL}
@@ -91,7 +198,7 @@ export default function MyTabs(props) {
                                     </Row>
                                 </ListItem>
                                 <ListItem>
-                                    <Row>
+                                    <Row onClick={()=>setRow_two(true)}>
                                         <Col>
                                             <ReactImg style={{ width: 30, height: 30 }}
                                                 src={item.ChampionAbility2_URL}
@@ -107,7 +214,7 @@ export default function MyTabs(props) {
                                     </Row>
                                 </ListItem>
                                 <ListItem>
-                                    <Row>
+                                    <Row onClick={()=>setRow_three(true)}>
                                         <Col>
                                             <ReactImg style={{ width: 30, height: 30 }}
                                                 src={item.ChampionAbility3_URL}
@@ -124,7 +231,7 @@ export default function MyTabs(props) {
                                     </Row>
                                 </ListItem>
                                 <ListItem>
-                                    <Row>
+                                    <Row onClick={()=>setRow_four(true)}>
                                         <Col><ReactImg style={{ width: 30, height: 30 }}
                                             src={item.ChampionAbility4_URL}
                                             loader={
@@ -138,7 +245,8 @@ export default function MyTabs(props) {
                                         <Col><strong>{item.Ability4}</strong></Col>
                                     </Row>
                                 </ListItem>
-                                <ListItem><Row>
+                                <ListItem>
+                                    <Row onClick={()=>setRow_fifth(true)}>
                                     <Col><ReactImg style={{ width: 30, height: 30 }}
                                         src={item.ChampionAbility5_URL}
                                         loader={
